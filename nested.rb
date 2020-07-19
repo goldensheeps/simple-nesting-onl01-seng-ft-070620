2b3ed160dfb74f48a38107decfd326316800d443
Timothy Lalli <trlalli92@gmail.com>
2020-07-19 16:32:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,11 +87,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-  programmer_hash[:yukihiro_matsumoto] = {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
-  programmer_hash
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
+programmer_hash
 
 end
 
